acca851519115daf384a6c6531bfeae5b7469877
kdaglas <daglach7@gmail.com>
2019-08-01 11:29:53 +0300

    Restructure the file structure

---
+++ src/movies.js
@@ -0,0 +1,37 @@
+db.movies.insert([
+  {
+    title: 'John Wick',
+    genre: 'action',
+    time: '2hrs 15mins',
+    description: 'A man fights against bad guys',
+    image: 'none',
+  },
+  {
+    title: 'Murder express',
+    genre: 'action',
+    time: '2hrs',
+    description: 'A woman owns a train where all assassines travel',
+    image: 'none',
+  },
+  {
+    title: 'Princess Switch',
+    genre: 'love',
+    time: '1hr 45mins',
+    description: 'Twins find themselves playing each others role',
+    image: 'none',
+  },
+  {
+    title: 'React Redux',
+    genre: 'documentary',
+    time: '45mins',
+    description: 'Teams of 5 learn what react can do',
+    image: 'none',
+  },
+  {
+    title: 'Miss Pelegrin',
+    genre: 'scifi',
+    time: '1hr 28mins',
+    description: 'A group of gifted kids are sheltered by another gifted lady',
+    image: 'none',
+  },
+]);
--- src/myapp.js
+++ src/myapp.js
@@ -1,8 +1,16 @@
 const express = require('express');
 
 const myapp = express();
-
 const port = process.env.PORT || 3000;
+const movieRouter = express.Router();
+
+movieRouter.route('/movies')
+  .get((req, res) => {
+    const response = { Hi: 'whatsup, have you seen spiderman' };
+    res.json(response);
+  });
+
+myapp.use('/api', movieRouter);
 
 myapp.get('/', (req, res) => {
   res.send('welcome to the movies and series API');
